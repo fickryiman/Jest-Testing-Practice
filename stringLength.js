798c16672e5049374d4738975f13e81092bacabc
Fickry Iman <fickry.iman@gmail.com>
2023-04-02 22:54:10 +0700

    task 1 is finished

--- stringLength.js
+++ stringLength.js
@@ -1,13 +1,9 @@
 function stringLength(string) {
-  if (string.length > 1 && string.length <= 10) {
+  if (string.length >= 1 && string.length <= 10) {
     return string.length;
   } else {
     throw new Error("the string arguments is at least 1 character long and not longer than 10 characters!");
   }
 }
 
-console.log(stringLength('fickry'));
-console.log(stringLength('bil iman'));
-console.log(stringLength('fickry bil iman'));
-
-export default stringLength;
+module.exports = stringLength;
